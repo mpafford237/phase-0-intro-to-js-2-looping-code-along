2544d4aeaa6ffc29a8f9b57fa9fde7aa848de54e
Mason Pafford <mdpafford2015@gmail.com>
2023-11-06 18:17:39 -0600

    Completed Loop Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function countDown(number) {
+  for (let index = number; index >= 0; index--) {
+    console.log(index);
+  }
+}
+  
+function writeCards(names, event) {
+  const messages = new Array();
+  for (let index = 0; index < names.length; index++) {
+    messages[index] = `Thank you, ${names[index]}, for the wonderful ${event} gift!`;
+  }
+  return messages;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
